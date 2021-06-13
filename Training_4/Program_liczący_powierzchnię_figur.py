3516b94149f282a8e0843ba8f74abf8cc928633f
Maciej Kaliś <mackal1993@gmail.com>
2021-06-13 16:22:58 +0200

    Adding enum function

--- Training_4/Program_liczący_powierzchnię_figur.py
+++ Training_4/Program_liczący_powierzchnię_figur.py
@@ -5,39 +5,44 @@ Program liczący powierzchnię wybranej z menu figury stworzony w celu przećwic
     
 import Figury
 
-jakaFigura = input("""Witaj w programie liczącym pole wybranej figury!
-Pole jakiej figury chcesz obliczyć?
-Prostokąt, kwadrat, trójkąt, trapez, koło.
-Wybierz figurę: """)
+from enum import IntEnum
 
-if jakaFigura == "prostokąt":
+Menu_Figury = IntEnum ('Menu_Figury', 'PROSTOKĄT KWADRAT TRÓJKĄT TRAPEZ KOŁO')
+
+jakaFigura = int(input("""Witaj w programie liczącym pole wybranej figury!
+Pole jakiej figury chcesz obliczyć?
+1 - Prostokąt, 2 - Kwadrat, 3 - Trójkąt, 4 - Trapez, 5 - Koło.
+Wybierz figurę: """))
+
+if jakaFigura == Menu_Figury.PROSTOKĄT:
     a = int(input("Podaj długość pierwszego boku prostokąta: ")) 
     b = int(input("Podaj długość drugiego boku prostokąta: "))
 
     print ("Pole prostokąta to:", Figury.pole_prostokata (a, b), "j^2")
 
-elif jakaFigura == "kwadrat":
+elif jakaFigura == Menu_Figury.KWADRAT:
     a = int(input("Podaj długość boku kwadratu: ")) 
     
     print ("Pole kwadratu to:", Figury.pole_kwadratu (a), "j^2")
 
-elif jakaFigura == "trójkąt":
+elif jakaFigura == Menu_Figury.TRÓJKĄT:
     a = int(input("Podaj długość podstawy trójkąta: ")) 
     h = int(input("Podaj wysokość trójkąta: "))
     
     print ("Pole trójkąta to:", Figury.pole_trojkata (a, h), "j^2")
 
-elif jakaFigura == "trapez":
+elif jakaFigura == Menu_Figury.TRAPEZ:
     a = int(input("Podaj długość dłuższej podstawy trapezu: "))
     b = int(input("Podaj długość krótszej podstawy trapezu: "))
     h = int(input("Podaj wysokość trapezu: "))
     
     print ("Pole trapezu to:", Figury.pole_trapezu (a, b, h), "j^2")
 
-elif jakaFigura == "koło":
+elif jakaFigura == Menu_Figury.KOŁO:
     r = int(input("Podaj promień koła: ")) 
     
     print ("Pole koła to:", Figury.pole_kola (r), "j^2")
 
 else:
     print ("Nie wybrałeś odpowiedniej figury")
+
