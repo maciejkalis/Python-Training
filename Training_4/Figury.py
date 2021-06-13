e7c0ecfba826c3be8f39124d701c0f203189d443
Maciej Kaliś <82841546+maciejkalis@users.noreply.github.com>
2021-06-13 11:55:22 +0200

    removing Polish diacritics

--- Training_4/Figury.py
+++ Training_4/Figury.py
@@ -5,17 +5,17 @@ Zbiór funkcji liczących pola kilku figur geometrycznych.
 
 import math
 
-def pole_prostokąta (a, b):
+def pole_prostokata (a, b):
     return a * b
 
 def pole_kwadratu (a):
     return a ** 2
 
-def pole_trójkąta (a, h):
+def pole_trojkata (a, h):
     return 0.5 * a * h
 
 def pole_trapezu (a, b, h):
     return ((a + b) * h) * 0.5
 
-def pole_koła (r):
+def pole_kola (r):
     return math.pi * (r ** 2)
