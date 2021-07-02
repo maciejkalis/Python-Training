af251725c1f69d28ced1affe14441fa64adfcd3a
Maciej Kaliś <mackal1993@gmail.com>
2021-07-02 16:18:18 +0200

    implementing function

--- Training_6/Site_access_validator.py
+++ Training_6/Site_access_validator.py
@@ -6,38 +6,44 @@ one for sites that can be reached and another for sites that cannot be reached.
 
 import requests
 
-siteList = []
+fileName = input("Type name of the file with sites listed for access validation: ")
 
-with open ("sites_test.txt", "r", encoding = "UTF-8") as file:
-    for line in file:
-        siteList.append(line.replace("\n", ""))
-
-validSites = []
-
-invalidSites = []
-
-try:
-    for site in siteList:
-        response = requests.get(site)
-        
-        if response.status_code == 200:
-            validSites.append(site)
-
-        elif response.status_code == 404:
-            invalidSites.append(site)
-                   
+def site_validator(fileName):
     
-except requests.exceptions.MissingSchema:
-    "Data in a format other than the URL present in the file"
+    siteList = []
+
+    with open ((fileName + ".txt"), "r", encoding = "UTF-8") as file:
+        for line in file:
+            siteList.append(line.replace("\n", ""))
+
+    validSites = []
+
+    invalidSites = []
+
+    try:
+        for site in siteList:
+            response = requests.get(site)
+            
+            if response.status_code == 200:
+                validSites.append(site)
+
+            elif response.status_code == 404:
+                invalidSites.append(site)
+                       
+        
+    except requests.exceptions.MissingSchema:
+        "Data in a format other than the URL present in the file"
 
 
-with open ("valid_sites.txt", "a+", encoding = "UTF-8") as file:
-    file.write("List of sites that can be reached:" + "\n")
-    for site in validSites:
-        file.write(site + "\n")
+    with open ("valid_sites.txt", "a+", encoding = "UTF-8") as file:
+        file.write("List of sites that can be reached:" + "\n")
+        for site in validSites:
+            file.write(site + "\n")
 
-with open ("invalid_sites.txt", "a+", encoding = "UTF-8") as file:
-    file.write("List of sites that cannot be reached:" + "\n")
-    for site in invalidSites:
-        file.write(site + "\n")   
+    with open ("invalid_sites.txt", "a+", encoding = "UTF-8") as file:
+        file.write("List of sites that cannot be reached:" + "\n")
+        for site in invalidSites:
+            file.write(site + "\n")
+
+site_validator(fileName)   
 
